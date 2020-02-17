4a424e89e247320054d706c4f3bb24f2ae03cd55
Matt Smith <43058533+m-smit474@users.noreply.github.com>
2020-02-17 13:28:48 -0700

    Added line animation
    line moves across the screen, then "bounces" off the other wall and comes back. Also has adjustable speed

--- GLIB.C
+++ GLIB.C
@@ -1,15 +1,60 @@
 #include <stdio.h>
+#include <linea.h>
 #include <osbind.h>
 
 #define BYTES_PER_SCREEN 32000
+#define SOLID 0xFFFF
+#define DOTTED 0xAAAA
+#define XOR 2
+#define FILLED -1
+#define BLANK 0
 
 void disable_cursor();
-void clear_screen(long *base, char pattern);
+void fill_screen(long *base, char pattern);
+void plot_line(unsigned short x1, unsigned short y1,
+				unsigned short x2, unsigned short y2,
+				short style, short mode);
+void send_block(int speed);
 
 int main() 
 {
+	send_block(1);
 
 	return 0;
+}
+
+void send_block(int speed)
+{
+	long *base = Physbase();
+	
+	int x = 0;
+	
+	disable_cursor();					/* hide cursor */
+	printf("\033E\033f\n");				/* Blank Screen */
+	linea0();
+	
+	while(!Cconis())					/* Until key press */
+	{
+		
+		while(x < 640 && !Cconis())
+		{
+			plot_line(x, 0, x, 399, SOLID, XOR); /* plot line */
+			Vsync();
+			plot_line(x, 0, x, 399, SOLID, XOR); /* unplot line */
+		
+			x+= speed;
+		}
+		
+		while(x > 0 && !Cconis())
+		{
+			plot_line(x, 0, x, 399, SOLID, XOR); /* plot line */
+			Vsync();
+			plot_line(x, 0, x, 399, SOLID, XOR); /* unplot line */
+			
+			x-= speed;
+		}
+	}
+	Cnecin();
 }
 
 void fill_screen(long *base, char pattern)
@@ -21,9 +66,18 @@ void fill_screen(long *base, char pattern)
 		*(location++) = pattern;
 }
 	
-int plot_line()
+void plot_line(unsigned short x1, unsigned short y1,
+				unsigned short x2, unsigned short y2,
+				short style, short mode)
 {
-	return 0;
+	X1 = x1;
+	Y1 = y1;
+	X2 = x2;
+	Y2 = y2;
+	LNMASK = style;
+	WMODE = mode;
+	LSTLIN = 0;
+	linea3();
 }
 
 void disable_cursor()
