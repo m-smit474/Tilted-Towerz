16c8f2d5efe7bb76a0ee586db57ae539403b97c0
Aylixx <60983701+Aylixx@users.noreply.github.com>
2020-02-25 14:27:48 -0700

    Update TST_RAST.C

--- TST_RAST.C
+++ TST_RAST.C
@@ -3,12 +3,17 @@
 
 int main()
 {
-	/*void *base = Physbase();
+	//void *base = Physbase();
 
-	plot_something_1(base, 100, 100);
+	/*plot_something_1(base, 100, 100);
 	plot_something_2(base, 200, 200);*/
 
-	draw_block(50, 50, 150);
+	//draw_block(base, 100, 100, 150);
+
+	UINT16 *base = Physbase();
+
+	plot_hline(base, 50, 50, 150);
+	
 
 	return 0;
 }
