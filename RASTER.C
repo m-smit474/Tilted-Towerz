b7196912489e0b9a8792e6352785f7465d5aa98a
Matt Smith <43058533+m-smit474@users.noreply.github.com>
2020-02-25 22:17:43 -0700

    Added vertical line plotting
    Also formatted raster.c

--- RASTER.C
+++ RASTER.C
@@ -7,6 +7,9 @@
 #define XOR 2
 #define SOLID 0xFFFF
 #define BITS_IN_BYTE 8
+#define BYTES_PER_SCREEN 32000
+#define BLACK -1
+#define WHITE 0
 
 /*void plot_something_1(UINT16 *base, int x, int y, ...) */
 /*{ */
@@ -50,31 +53,71 @@ void disable_cursor()
 	fflush(stdout);
 }
 
+void fill_screen(UINT16 *base, char pattern)
+{
+	register int i = 0;
+	register UINT16 *location = base;
+	
+	while (i++ < BYTES_PER_SCREEN)
+		*(location++) = pattern;
+}
+
 void plot_hline (UINT16 *base, int y, int x1, int x2)
 {
-UINT16 p1, p2;
-int row1, row2, i;
-int shift_F, shift_B;
-row1 = x1 / BITS_IN_BYTE;
-row2 = x2 / BITS_IN_BYTE;
-shift_F = x1 >> 3;
-shift_B = (BITS_IN_BYTE - 1) - (x2 % (BITS_IN_BYTE - 1));
-if (row1 == row2)
-{
-p1 = SOLID >> shift_F;
-p1 = SOLID << shift_B;
-*(base + y * 80 + row1) = p1;
+	UINT16 p1, p2;
+	int row1, row2, i;
+	int shift_F, shift_B;
+	
+	row1 = x1 / BITS_IN_BYTE;
+	row2 = x2 / BITS_IN_BYTE;
+	shift_F = x1 >> 3;
+	shift_B = (BITS_IN_BYTE - 1) - (x2 % (BITS_IN_BYTE - 1));
+	
+	if (row1 == row2)
+	{
+		p1 = SOLID >> shift_F;
+		p1 = SOLID << shift_B;
+		*(base + y * 80 + row1) = p1;
+	}
+	else
+	{
+		p1 = SOLID >> shift_F;
+		p2 = SOLID << shift_B;
+		*(base + y * 80 + row1) = p1;
+		for (i = row1 + 1; i < row2; i++)
+		{
+			*(base + y * 80 + i) = SOLID;
+		}
+		*(base + y * 80 + row2) = p2;
+	}
+	
+	return;
 }
-else
+
+void plot_vline (UINT16 *base, int x, int y1, int y2)
 {
-p1 = SOLID >> shift_F;
-p2 = SOLID << shift_B;
-*(base + y * 80 + row1) = p1;
-for (i = row1 + 1; i < row2; i++)
-{
-*(base + y * 80 + i) = SOLID;
-}
-*(base + y * 80 + row2) = p2;
-}
-return;
+	int temp;
+	UINT16 pattern;
+	UINT16 *screen_byte;
+	
+	if (x >= 0 && x < 640)
+	{
+		if (y1 > y2)
+		{
+		temp = y1;
+		y1 = y2;
+		y2 = temp;
+		}
+		if (y1 < 0) y1 = 0;
+		if (y2 > 399) y2 = 399;
+		
+		pattern = 1 << (7 - (x & 7));
+		screen_byte = base + y1 * 80 + (x >> 3);
+		for ( ; y1 <= y2; y1++)
+		{
+		*screen_byte = pattern;
+		screen_byte = screen_byte + 80;
+		}
+	}
+	return;
 }
--- TST_RAST.C
+++ TST_RAST.C
@@ -3,16 +3,20 @@
 
 int main()
 {
-	//void *base = Physbase();
+	/*void *base = Physbase();
 
-	/*plot_something_1(base, 100, 100);
-	plot_something_2(base, 200, 200);*/
+	plot_something_1(base, 100, 100);
+	plot_something_2(base, 200, 200);
 
-	//draw_block(base, 100, 100, 150);
+	draw_block(base, 100, 100, 150);*/
 
 	UINT16 *base = Physbase();
+	
+	fill_screen(base, 0);
 
 	plot_hline(base, 50, 50, 150);
+	
+	plot_vline(base, 50, 50, 150);
 	
 
 	return 0;
