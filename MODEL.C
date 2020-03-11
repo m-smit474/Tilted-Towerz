38aceb8f4083373e0f2e0fb34ee2a32a67a2290b
Matt Smith <43058533+m-smit474@users.noreply.github.com>
2020-03-11 16:22:20 -0600

    Updated Model

--- MODEL.C
+++ MODEL.C
@@ -2,14 +2,21 @@
 #include "raster.h"
 #include <stdio.h>
 
+#define DROP_SPEED 5
+
 void move_block_h(struct Block *block)
 {
-	block->x += block->delta_x;
+	block->x += block->speed;
 }
 
 void move_block_v(struct Block* block)
 {
-	block->y += block->delta_y;
+	block->y += block->DROP_SPEED;
 
 	/*colliosion detection here*/
+}
+
+void update_size()
+{
+	
 }
