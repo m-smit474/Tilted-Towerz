1e96a0b41d006755ca8823e82c37a87a192f5bc6
Matt Smith <43058533+m-smit474@users.noreply.github.com>
2020-03-11 17:19:50 -0600

    Update Size function

--- MODEL.C
+++ MODEL.C
@@ -1,5 +1,6 @@
 #include "model.h"
 #include "raster.h"
+#include "events.h"
 #include <stdio.h>
 
 #define DROP_SPEED 5
@@ -12,11 +13,18 @@ void move_block_h(struct Block *block)
 void move_block_v(struct Block* block)
 {
 	block->y += block->DROP_SPEED;
-
-	/*colliosion detection here*/
 }
 
-void update_size()
+void update_size(struct Block* block, int previousLeft)
 {
+	int displacement = previousLeft - (block->x);
 	
+	displacement = abs(displacement);
+	
+	block->length = block->length - displacement;
+	
+	if(block->x < previousLeft)
+	{
+		block->x = block->x + displacement;
+	}
 }
