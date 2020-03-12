e2cea2fb0cff6a7ba55d8f12fa215308a962d3f9
Matt Smith <43058533+m-smit474@users.noreply.github.com>
2020-03-12 11:39:47 -0600

    Added test module for Events & Module

---
+++ TST_MOD.C
@@ -0,0 +1,14 @@
+#include "model.h"
+#include "events.h"
+
+int main()
+{
+	Block testBlock;
+	
+	testBlock.x = 50;
+	testBlock.y = 50;
+	testBlock.speed = 1;
+	testBlock.length = 50;
+	
+	return 0;
+}
