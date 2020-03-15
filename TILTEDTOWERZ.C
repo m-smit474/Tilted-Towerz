14cffbe095982db3a7ffd4271cb096c042798ccc
Matt Smith <43058533+m-smit474@users.noreply.github.com>
2020-03-15 14:37:31 -0600

    Filled in renderer.h

--- TILTEDTOWERZ.C
+++ TILTEDTOWERZ.C
@@ -7,12 +7,13 @@
 int main() 
 {
 	Block block;
+	UINT16 *base = physbase();
 
 	block->x = 50;
 	block->y = 50;
 	block->speed = 0;
 	block->length = 50;
 
-	render_block(block, UINT16 *base);
+	render_block(block, base);
 
 }
