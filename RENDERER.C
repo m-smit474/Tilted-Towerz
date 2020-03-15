83a30a7fe41d0e34288d7fe3ba157e1c4f78ce22
Matt Smith <43058533+m-smit474@users.noreply.github.com>
2020-03-15 13:57:50 -0600

    Added functions to Renderer
    Also updated Model.H to include a Model struct which contains all previous blocks

---
+++ RENDERER.C
@@ -0,0 +1,20 @@
+include "model.h"
+include "raster.h"
+
+void render(const struct Model *model, UINT16 *base)
+{
+	int currentBlock = 0;
+	
+	while(currentBlock < model->fillLevel)
+	{
+		render_block(model->blocks[currentBlock]);
+		
+		currentBlock++;
+	}
+	
+}
+
+void render_block(const struct *Block block, UINT16 *base)
+{
+	draw_block(base, block->x, block->y, block->length);
+}
